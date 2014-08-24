8b6ad187d0b114e5dd136f605758ace6524ad48c
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2014-08-24 22:00:10 +0300

    Fix clear color

--- core/src/com/m4t1a5/ld30/LD30Game.java
+++ core/src/com/m4t1a5/ld30/LD30Game.java
@@ -97,7 +97,7 @@ public class LD30Game extends ApplicationAdapter
         batch.setProjectionMatrix(camera.projection);
         batch.setTransformMatrix(camera.view);
 
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
 
